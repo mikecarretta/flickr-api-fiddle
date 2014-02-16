1a34d8f892d01f01d0c7510d26b49b4e666a054f
Mike Carretta <mikecarretta@gmail.com>
2014-02-16 16:24:52 -0500

    Re-design and started v2

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ var flickrKey = '31749d85ae9c0811522eedcac9b7390d',
 
 // Create the FindByUsername URL
 var usernameBaseURL = 'http://api.flickr.com/services/rest/',
-	flickrFindByUsernameURL = usernameBaseURL + '?method=flickr.people.getInfo' 
+	flickrFindByUsernameURL = usernameBaseURL + '?method=flickr.people.getInfo'
 		+ '&api_key=' + flickrKey + '&user_id=' + flickrUserID
 		+ '&format=json&nojsoncallback=1';
 // console.log(flickUrl);
@@ -36,7 +36,7 @@ var photosBaseURL = 'http://api.flickr.com/services/rest/?method=flickr.people.g
 		photosPerPage + '&page=' + photoPages + '&format=json&nojsoncallback=1';
 // console.log(PublicPhotosURL);
 
-/* 
+/*
 	* flickr.people.getInfo
 	* http://www.flickr.com/services/api/flickr.people.getInfo.html
 */
@@ -47,20 +47,20 @@ var flickrFindByUsername = function() {
 		success: function(data) {
 
 			var flickrInfo = $( "#flickrUserInfo" );
-			$('<h2><a href="' 
-				+ data.person.profileurl._content 
+			$('<h1><a href="'
+				+ data.person.profileurl._content
 				+ '">'
-				+ data.person.realname._content 
-				+ '</a></h2>').appendTo(flickrInfo);
+				+ data.person.realname._content
+				+ '</a></h1>').appendTo(flickrInfo);
 
-			$('<p>' 
-				+ data.person.id 
-				+ ', ' 
-				+ data.person.username._content 
+			$('<p>'
+				+ data.person.id
+				+ ', '
+				+ data.person.username._content
 				+ '</p>').appendTo(flickrInfo);
 
-			$('<p>' 
-				+ data.person.description._content 
+			$('<p>'
+				+ data.person.description._content
 				+ '</p>').appendTo(flickrInfo);
 		},
 		error: function(error) {
@@ -69,7 +69,7 @@ var flickrFindByUsername = function() {
 	});
 }
 
-/* 
+/*
 	* flickr.people.getPublicPhotos
 	* http://www.flickr.com/services/api/flickr.people.getPhotos.html
 	* http://www.flickr.com/photos/{user-id}/{photo-id} - individual photo
@@ -82,23 +82,23 @@ var flickrGetPublicPhotos = function() {
 			// console.log(data);
 			// Iterates over the properties in an object, accessing both the current item and its key.
 			$.each(data.photos.photo, function(k, v) {
-				
+
 				var flickrInfo = $( "#flickrPhotos" );
 
-				$('<div class="thumb"><a href="http://www.flickr.com/photos/' 
-					+ v.owner + '/' 
-					+ v.id 
-					+ '"><img src="http://farm' 
-					+ v.farm 
-					+ '.staticflickr.com/' 
-					+ v.server 
-					+ '/' 
-					+ v.id 
-					+ '_' 
-					+ v.secret 
-					+ '_n.jpg' 
-					+ '" id="' 
-					+ v.id 
+				$('<div class="thumb"><a href="http://www.flickr.com/photos/'
+					+ v.owner + '/'
+					+ v.id
+					+ '"><img src="http://farm'
+					+ v.farm
+					+ '.staticflickr.com/'
+					+ v.server
+					+ '/'
+					+ v.id
+					+ '_'
+					+ v.secret
+					+ '_n.jpg'
+					+ '" id="'
+					+ v.id
 					+ '"><div class="mask"><p>'
 					+ v.title
 					+ '</p></div></a></div>'
@@ -107,7 +107,7 @@ var flickrGetPublicPhotos = function() {
 		},
 		error: function(error) {
 			console.log("Error: "+ error.status);
-		}, 
+		},
 	});
 }
 
